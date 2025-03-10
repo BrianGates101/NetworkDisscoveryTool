c9cd99893e52b3900daceca190025254cfb5cd76
Brian Gates <briangates76@gmail.com>
2025-03-10 21:25:15 +0200

    Fixed MAC and Vendor discovery and added HostName discovery. And made things better with -sP -PR instead of -sn. also added loop to discover over a few iterations to miss less devices. (also made the output a table)

--- DiscoverNodes.py
+++ DiscoverNodes.py
@@ -1,20 +1,27 @@
 import nmap
+import time
 
-def discover_devices(network):
+def discover_devices(network, retries=3, delay=5):
     nm = nmap.PortScanner()
-    nm.scan(hosts=network, arguments='-sn')
     devices = []
-    for host in nm.all_hosts():
-        if 'mac' in nm[host]['addresses']:
+
+    for attempt in range(retries):
+        nm.scan(hosts=network, arguments='-sP -PR')
+        for host in nm.all_hosts():
+            # print(f"Debug: Host {host} data: {nm[host]}")  # Debug statement to print host data
+            ip_address = nm[host]['addresses'].get('ipv4', '')
+            mac_address = nm[host]['addresses'].get('mac', '')
+            vendor = nm[host]['vendor'].get(mac_address, '')
+            host_name = nm[host]['hostnames'][0]['name'] if nm[host]['hostnames'] != '' else ''
+            
             devices.append({
-                'ip': nm[host]['addresses']['ipv4'],
-                'mac': nm[host]['addresses']['mac'],
-                'vendor': nm[host]['vendor'].get(nm[host]['addresses']['mac'], 'Unknown')
+                'ip': ip_address,
+                'mac': mac_address,
+                'vendor': vendor,
+                'host_name': host_name
             })
-        else:
-            devices.append({
-                'ip': nm[host]['addresses']['ipv4'],
-                'mac': 'Unknown',
-                'vendor': 'Unknown'
-            })
-    return devices
+        time.sleep(delay)  # Wait before the next scan attempt
+
+    # Remove duplicates based on IP address
+    unique_devices = {device['ip']: device for device in devices}.values()
+    return list(unique_devices)
