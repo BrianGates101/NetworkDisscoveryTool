e8014d931083d5c4ab2542754562fa01d338cd8f
Brian Gates <briangates76@gmail.com>
2025-03-10 19:00:04 +0200

    Removed the useless stuff at the top of main (also testing a commit)

--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-# import logging
-# from DiscoverNodes import discover_devices
-
-# # Configure logging
-# logging.basicConfig(filename='errlog.log', level=logging.ERROR)
-
-# if __name__ == "__main__":
-#     try:
-#         network = '192.168.0.0/24'
-#         devices = discover_devices(network)
-#         for device in devices:
-#             print(f"IP: {device['ip']}, MAC: {device['mac']}, Vendor: {device['vendor']}")
-#     except Exception as e:
-#         logging.error("An error occurred", exc_info=True)
-
-
-
-
 import logging
 from DiscoverNodes import discover_devices
 
